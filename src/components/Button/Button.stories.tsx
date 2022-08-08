78787b09883948d9d4bf2c0537e3eb56a94712cd
Konstantin Zhitkov <kzhitkov@adsterra.com>
2022-08-08 19:25:31 +0300

    Align `Button` component to UI Kit

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { Meta } from "@storybook/react/types-6-0";
 import { Story } from "@storybook/react";
-import Button, { ButtonProps } from "./Button";
+import Button from "./Button";
+import { ButtonProps } from "./types";
 
 export default {
   title: "Components/Button",
   component: Button,
   argTypes: {
-    backgroundColor: { control: "color" },
-  },
+    size: {
+      options: ['medium', 'large'],
+      control: { type: 'select' },
+    },
+  }
 } as Meta;
 
 // Create a master template for mapping args to render the Button component
@@ -16,7 +20,7 @@ const Template: Story<ButtonProps> = (args) => <Button {...args} />;
 
 // Reuse that template for creating different stories
 export const Primary = Template.bind({});
-Primary.args = { label: "Primary", size: "large" };
+Primary.args = { label: "Primary", primary: true, size: "medium" };
 
 export const Secondary = Template.bind({});
-Secondary.args = { ...Primary.args, primary: false, label: "Secondary" };
+Secondary.args = { ...Primary.args, primary: false, label: "Secondary", size: "medium" };
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,49 +1,23 @@
 import React from "react";
-import "./button.css";
+import { ButtonProps } from "./types";
+import "./styles.css";
 
-export interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * How large should the button be?
-   */
-  size?: "small" | "medium" | "large";
-  /**
-   * Button contents
-   */
-  label: string;
-  /**
-   * Optional click handler
-   */
-  onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
-}
-
-/**
- * Primary UI component for user interaction
- */
+// Primary UI component for user interaction
 const Button = ({
   primary = true,
-  backgroundColor,
   size = "medium",
   onClick,
   label,
 }: ButtonProps) => {
   const mode = primary
-    ? "storybook-button--primary"
-    : "storybook-button--secondary";
+    ? "button--primary"
+    : "button--secondary";
+  const classNames = ["button", `button--${size}`, mode].join(' ');
+  
   return (
     <button
       type="button"
-      className={["storybook-button", `storybook-button--${size}`, mode].join(
-        " "
-      )}
-      style={backgroundColor ? { backgroundColor } : {}}
+      className={classNames}
       onClick={onClick}
     >
       {label}
